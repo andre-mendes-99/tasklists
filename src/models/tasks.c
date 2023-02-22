457c72f8210f70c36d2c41e95b753ba3ef1c224d
andre-mendes-99 <andrezmendes18@gmail.com>
2023-02-22 16:04:10 +0000

    aula

--- src/models/tasks.c
+++ src/models/tasks.c
@@ -2,40 +2,55 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Task_ {
-    char* id;
-    char* description;
-    char* status;
+struct Task_
+{
+    char *id;
+    char *description;
+    char *status;
 };
 
-Task task_new(char* id, char* description) {
+Task task_new(char *id, char *description)
+{
     Task task = malloc(sizeof(struct Task_));
     task->id = strdup(id);
-    task->description = strdup(description);
+    if (description != NULL)
+    {
+        task->description = strdup(description);
+    }
+    else
+    {
+        description = NULL;
+    }
+
     task->status = strdup("Por completar");
     return task;
 }
 
-void task_destroy(Task task) {
+void task_destroy(Task task)
+{
     free(task->id);
     free(task->description);
     free(task->status);
     free(task);
 }
 
-char* task_get_id(Task task) {
+char *task_get_id(Task task)
+{
     return task->id;
 }
 
-char* task_get_description(Task task) {
+char *task_get_description(Task task)
+{
     return task->description;
 }
 
-char* task_get_status(Task task) {
+char *task_get_status(Task task)
+{
     return task->status;
 }
 
-void task_set_completed(Task task) {
+void task_set_completed(Task task)
+{
     free(task->status);
     task->status = strdup("Completa");
 }
--- src/tests/test_tasks.c
+++ src/tests/test_tasks.c
@@ -23,9 +23,19 @@ void test_task_get_id() {
     TEST_ASSERT_EQUAL_STRING("TESTE", task_get_id(task));
 }
 
+void test_task_get_description(){
+    Task task = task_new("1", "description");
+    TEST_ASSERT_EQUAL_STRING("description", task_get_description(task));
+    task_destroy(task);
+    task = task_new("1", NULL);
+    TEST_ASSERT_NULL(task_get_description(task));
+    task_destroy(task);
+}
+
 int main() {
     UNITY_BEGIN();
     RUN_TEST(test_task_new);
     RUN_TEST(test_task_get_id);
+    RUN_TEST(test_task_get_description);
     return UNITY_END();
 }
